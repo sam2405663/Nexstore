53249f7c697727fce1ae5880131b6726c6e325cc
sam2405663 <samg58168@gmail.com>
2026-09-17 01:55:36 -0700

    new

--- backend/server.js
+++ backend/server.js
@@ -66,10 +66,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/admin", adminRoutes);
 
+// Production Static Serving
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/{*splat}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
